6c63f35bbe18ab099752e3a914384be7e3d68c4c
dfbautista <daniel.bautista@vai.org>
2025-09-04 21:51:11 -0400

    Fix header script, change merge.sh

--- bin/Merge.sh
+++ bin/Merge.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# for f in $( ls PR001746_JAND_sciCT_mod/*.fastq.gz) ; do
-#     echo "cat PR001746_JAND_sciCT_mod/${f} PR001746_JAND_sciCT_2_mod/${f} > PR001746_JAND_sciCT_merged_mod/${f}"
-# done
-
-
-cat PR001746_JAND_sciCT_mod/DJCoCnT060625_S2_L002_R1_001.fastq.gz PR001746_JAND_sciCT_2_mod/DJCoCnT060625_L000_R1_001.fastq.gz \
-  > PR001746_JAND_sciCT_merged_mod/DJCoCnT060625_L002_R1_001.fastq.gz
-
-cat PR001746_JAND_sciCT_mod/DJCoCnT060625_S2_L002_R2_001.fastq.gz PR001746_JAND_sciCT_2_mod/DJCoCnT060625_L000_R2_001.fastq.gz \
-  > PR001746_JAND_sciCT_merged_mod/DJCoCnT060625_L002_R2_001.fastq.gz
-
-cat PR001746_JAND_sciCT_mod/DJCoCnT060625_UMI_S2_L002_I1_001.fastq.gz PR001746_JAND_sciCT_2_mod/DJCoCnT060625_UMI_S1_L001_I1_001.fastq.gz \
-  PR001746_JAND_sciCT_2_mod/DJCoCnT060625_UMI_S1_L002_I1_001.fastq.gz > PR001746_JAND_sciCT_merged_mod/DJCoCnT060625_UMI_L000_I1_001.fastq.gz
-
-cat PR001746_JAND_sciCT_mod/DJCoCnT060625_UMI_S2_L002_I2_001.fastq.gz PR001746_JAND_sciCT_2_mod/DJCoCnT060625_UMI_S1_L001_I2_001.fastq.gz \
-  PR001746_JAND_sciCT_2_mod/DJCoCnT060625_UMI_S1_L002_I2_001.fastq.gz > PR001746_JAND_sciCT_merged_mod/DJCoCnT060625_UMI_L000_I2_001.fastq.gz
+DirName1="$1"
+DirName2="$2"
+mkdir -p merged
+for f in $( ls ${DirName1}/*.fastq.gz) ; do
+    name=$(basename ${f})
+    cat ${f} ${DirName2}/${name} > merged/${name}
+done
--- bin/ModifyHeader.sh
+++ bin/ModifyHeader.sh
@@ -4,7 +4,7 @@ module load bbc2/pigz/pigz-2.7
 
 FileName="$1"
 OutName="$2"
-read_pair=${BaseName#*_[R,I]}   # Remove everything up to and including _R or _I
+read_pair=${FileName#*_[R,I]}   # Remove everything up to and including _R or _I
 read_pair=${read_pair%%_*}  # Remove everything from next _ onwards
 
 pigz -dc ${FileName}  | \
